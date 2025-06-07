97d83aeee9d9645ce459ef8c7812d9bf7f83b557
ZENG <74520775+hashtag47@users.noreply.github.com>
2025-06-08 00:37:40 +0800

    Create sign-up-form and button component

--- cloth-web/src/routes/sign-in/sign-in.component.jsx
+++ cloth-web/src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,21 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+//signInWithGoogleRedirect has some issues which I couldn't solve at this stage
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await getRedirectResult(auth);
+  //     console.log(response);
+  //   }
+  //   fetchData();
+  // }, []);
+
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(response.user);
@@ -11,6 +25,10 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
+      <SignUpForm />
     </div>
   );
 };
