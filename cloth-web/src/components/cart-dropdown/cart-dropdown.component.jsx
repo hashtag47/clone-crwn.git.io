4f9d1fee23c9f9e6ed0739949da9a3b62dcc42ff
ZENG <74520775+hashtag47@users.noreply.github.com>
2025-06-11 20:29:07 +0800

    Add in our addToChar feature

--- cloth-web/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ cloth-web/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../../components/button/button.component";
-
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropDown = () => {
-  const { cart } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button buttonType="inverted">GO TO CHECKOUT</Button>
     </div>
   );
